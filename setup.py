dcaf9b8e101e0ba7da4d5f177211bb8dcd72b657
Ludovico Bianchi <lbianchi@lbl.gov>
2022-06-23 17:24:06 -0500

    Adopt setuptools_scm for packaging and versioning

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+
+setup(
+    name="dispatches-sample-data",
+    description="Sample datasets for the DISPATCHES project",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+)
