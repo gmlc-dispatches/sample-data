1abb0aecde54957b7064c237b4605c76fb0917fa
Ludovico Bianchi <lbianchi@lbl.gov>
2022-06-24 19:24:46 -0500

    Ensure all files are copied for both sdist and bdist_wheel

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description="Sample datasets for the DISPATCHES project",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
+    include_package_data=True,
     install_requires=[
         "importlib-metadata; python_version < '3.8'",
     ]
